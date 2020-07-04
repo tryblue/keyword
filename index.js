6a519fc3a992eebd679db0ac64cca4aa0fda1b7f
Alistair Smith <alistairsmith01@gmail.com>
2020-07-04 13:27:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const matchKeyword = (productName, keywords) => {
+  const name = productName.toLowerCase().trim();
+  return (
+    keywords
+      .toLowerCase()
+      .split(",")
+      .map((keyword) => keyword.trim())
+      .filter(
+        (word) =>
+          (word.includes("+") && !name.includes(word.substr(1))) ||
+          (word.includes("-") && name.includes(word.substr(1)))
+      ).length === 0
+  );
+};
