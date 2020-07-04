1af70c07883b4fc19105f802a0d82995a11e56f2
Alistair Smith <alistairsmith01@gmail.com>
2020-07-04 17:33:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Matches keywords to a product
  * @param {string} productName The name of the product
  * @param {string} keywords The keywords in +, - format
+ * @example
+ * const product = products.find(product => matchKeyword(product.name, '+T-Shirt, -Hoodie'));
  */
 const matchKeyword = (productName, keywords) => {
   const name = productName.toLowerCase().trim();
