8b3be8071915ce2d13d05a82e05ac538b690137a
Alistair Smith <alistairsmith01@gmail.com>
2020-08-25 10:09:13 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
-/**
- * Matches keywords to a product
- * @param productName The name of the product
- * @param keywords The keywords in +, - format
- */
-declare function matchKeyword(productName: string, keywords: string): boolean;
+declare module 'blue-keyword' {
+  /**
+   * Matches keywords to a product
+   * @param productName The name of the product
+   * @param keywords The keywords in +, - format
+   */
+  function matchKeyword(productName: string, keywords: string): boolean;
+  export = matchKeyword;
+}
